46b590f6a50ea0a5cca789022d7c595e924f5b7c
MartinCoimbra <63676653+MartinCoimbra@users.noreply.github.com>
2021-06-15 04:57:04 -0700

    Update ormconfig.js

--- ormconfig.js
+++ ormconfig.js
@@ -3,13 +3,18 @@ dotenv.config({override: true})// load .env variables
 
 console.log("process.env.DATABASE_URL", process.env.DATABASE_URL)
 module.exports = {
-	type: "postgres",
-	url: process.env.DATABASE_URL,
-	entities: ["./dist/entities/*.js"],
-	logging: false,
-    synchronize: process.env.NODE_ENV === 'development',
+ type: "postgres",
+ url: process.env.DATABASE_URL,
+ entities: ["./dist/entities/*.js"],
+ logging: false,
+    synchronize: true,
     migrations: ["./dist/migrations/*.js"],
     cli: {
         "migrationsDir": "./dist/migrations"
-    }
-}
+    },
+    // ssl: process.env.NODE_ENV === 'development' ? true : false
+    ssl: process.env.NODE_ENV === 'development' ? false: {
+        require: true,
+          rejectUnauthorized: false,
+        },
+ }
